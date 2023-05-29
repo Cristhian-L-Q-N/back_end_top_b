1b0427be641365b82b94b1c8ed099b1799dc3c35
unknown <tecn0crisis0@gmail.com>
2023-05-29 16:06:27 -0400

    7U7 2

--- querys.py
+++ querys.py
@@ -3,23 +3,6 @@ import pymysql
 from database import connect_to_database
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask import Flask, jsonify, request
-async def get_employees():
-    connection = await connect_to_database()
-    if connection:
-        try:
-            async with connection.cursor() as cursor:
-                sql = "SELECT * FROM Employees"
-                await cursor.execute(sql)
-                employees = await cursor.fetchall()
-                return employees
-        
-        except aiomysql.Error as e:
-            print(f"Error al obtener empleados: {e}")
-        
-        finally:
-            connection.close()
-
-# Agrega aquí más funciones de consultas según tus necesidades
 
 async def create_user(data):
     connection = await connect_to_database()
@@ -41,21 +24,5 @@ async def create_user(data):
         except aiomysql.Error as e:
             return {"error": str(e)}
         
-        finally:
-            connection.close()
-
-async def get_empleados():
-    connection=await connect_to_database()
-    if connection:
-        try:
-            with connection.cursor() as cursor:
-                sql = "SELECT ID_Empleado, Nombre, Usuario, Contrasena, Rol FROM Empleado"
-                cursor.execute(sql)
-                empleados = cursor.fetchall()
-
-                resultados = [{"id_empleado":empleado['ID_Empleado'],"nombre": empleado['Nombre'], "usuario": empleado['Usuario'], "contrasena": empleado['Contrasena'], "rol": empleado['Rol']} for empleado in empleados]
-                return jsonify(resultados)            
-        except pymysql.Error as e:
-            return jsonify({"error":"DataBase Error :{}".format(e)}),500
         finally:
             connection.close()
